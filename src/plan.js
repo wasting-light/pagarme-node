5d5ef07897e4dc9238f6335207ebd5f728eb13f4
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-12-27 21:54:07 -0200

    Create plan structure
    closes grvcoelho/pagarme-node#3

---
+++ src/plan.js
@@ -0,0 +1,28 @@
+import is from 'check-types';
+
+import Pagarme from './pagarme';
+import Request from './request';
+
+class Plan {
+	constructor(plan) {
+
+	}
+
+	save() {
+	
+	}
+
+	findAll(query) {
+
+	}
+
+	find(id) {
+
+	}
+
+	update() {
+	
+	}
+}
+
+export default Plan;
---
+++ test/plan.js
@@ -0,0 +1,10 @@
+import assert from 'power-assert';
+import is from 'check-types'
+
+import Pagarme from '../src/pagarme';
+import Plan from '../src/plan';
+
+describe('Request', () => {
+	beforeEach(() => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
+});
+
