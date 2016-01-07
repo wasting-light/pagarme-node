2bc344ac22fb0b00952cc9658bf4fd77c5a8e2fb
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-07 11:32:27 -0200

    Add descriptive message for pagarme
    closes grvcoelho/pagarme-node#19

--- src/pagarme.js
+++ src/pagarme.js
@@ -3,7 +3,7 @@ import is from 'check-types';
 class Pagarme {
 	static setApiKey(key) {
 		if (is.not.assigned(key) || is.not.string(key)) {
-			throw new Error();
+			throw new Error('You must provide an api key');
 		}
 
 		process.env.PAGARME_AK = key;
