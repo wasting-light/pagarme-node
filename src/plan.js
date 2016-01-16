2cb67171ae44f6dca9635f314e9330bd83dfeb82
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-16 14:53:34 -0200

    Fix linting and indentation on source files
    grvcoelho/pagarme-node#25

--- src/plan.js
+++ src/plan.js
@@ -1,62 +1,60 @@
 import is from 'check-types';
 
-import Pagarme from './pagarme';
 import Request from './request';
 
 let request;
 
 class Plan {
-    constructor(plan) {
-        Object.assign(this, plan);
-        request = new Request();
-    }
+	constructor(plan) {
+		Object.assign(this, plan);
+		request = new Request();
+	}
 
-    create() {
-        if (is.undefined(this.amount)) {
-            throw new Error('The plan must have the property amount');
-        }
+	create() {
+		if (is.undefined(this.amount)) {
+			throw new Error('The plan must have the property amount');
+		}
 
-        if (is.undefined(this.days)) {
-            throw new Error('The plan must have the property days'); 
-        }
+		if (is.undefined(this.days)) {
+			throw new Error('The plan must have the property days');
+		}
 
-        if (is.undefined(this.name)) {
-            throw new Error('The plan must have the property name');
-        }
+		if (is.undefined(this.name)) {
+			throw new Error('The plan must have the property name');
+		}
 
-        if (is.not.undefined(this.id)) {
-            throw new Error('The plan can\'t have the property id');
-        }
+		if (is.not.undefined(this.id)) {
+			throw new Error('The plan can\'t have the property id');
+		}
 
-        return request.post('plans', this)
-            .then(plan => {
-                Object.assign(this, plan)
-                return plan;
-            });
-    }
+		return request.post('plans', this)
+			.then(plan => {
+				Object.assign(this, plan);
+				return plan;
+			});
+	}
 
-    findAll(query) {
-        return request.get('plans', query);	
-    }
+	findAll(query) {
+		return request.get('plans', query);
+	}
 
-    find(id) {
-        id = id || this.id;
+	find(planId) {
+		const id = planId || this.id;
 
-        if (is.undefined(id)) {
-            throw new Error('You must provide an id');
-        }
+		if (is.undefined(id)) {
+			throw new Error('You must provide an id');
+		}
 
-        return request.get('plans/' + id, this)
-            .then(plan => {
-                Object.assign(this, plan)
-                return plan;
-            });
+		return request.get('plans/' + id, this)
+			.then(plan => {
+				Object.assign(this, plan);
+				return plan;
+			});
+	}
 
-    }
-
-    update(properties) {
-        return request.put('plans/' + this.id, properties);    
-    }
+	update(properties) {
+		return request.put('plans/' + this.id, properties);
+	}
 }
 
 export default Plan;
