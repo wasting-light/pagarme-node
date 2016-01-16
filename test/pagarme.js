b01900db7a677d99ad0a3943da4d0d0cc60f5321
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-01-16 14:57:08 -0200

    Fix linting and indentation on test files
    grvcoelho/pagarme-node#25

--- test/pagarme.js
+++ test/pagarme.js
@@ -1,23 +1,22 @@
 import 'babel-register';
-import is from 'check-types';
 import test from 'ava';
 
 import Pagarme from '../src/pagarme';
 
-test.beforeEach(t => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
+test.beforeEach(() => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
 
 test('set an api key', t => {
-    t.doesNotThrow(() => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
+	t.doesNotThrow(() => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
 });
 
 test('set an empty api key', t => {
-    t.throws(() => Pagarme.setApiKey());
+	t.throws(() => Pagarme.setApiKey());
 });
 
 test('set a non-string api key', t => {
-    t.throws(() => Pagarme.setApiKey([2, 3]));
+	t.throws(() => Pagarme.setApiKey([2, 3]));
 });
 
 test('get an api key', t => {
-    t.is(Pagarme.getApiKey(), 'ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9');
+	t.is(Pagarme.getApiKey(), 'ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9');
 });
--- test/plan.js
+++ test/plan.js
@@ -5,78 +5,78 @@ import test from 'ava';
 import Pagarme from '../src/pagarme';
 import Plan from '../src/plan';
 
-test.beforeEach(t => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
+test.beforeEach(() => Pagarme.setApiKey('ak_test_TSgC3nvXtdYnDoGKgNLIOfk3TFfkl9'));
 
 test('create a plan', async t => {
-    let plan = new Plan({
-        amount: 1000,
-        days: 30,
-        name: 'test-api'
-    });
+	const plan = new Plan({
+		amount: 1000,
+		days: 30,
+		name: 'test-api'
+	});
 
-    let p = await plan.create();
+	const p = await plan.create();
 
-    t.ok(is.not.undefined(p));
-    t.ok(is.object(p));
-    t.is(p.object, 'plan');
-    t.is(p.amount, 1000);
-    t.is(p.days, 30);
-    t.is(p.name, 'test-api');
+	t.ok(is.not.undefined(p));
+	t.ok(is.object(p));
+	t.is(p.object, 'plan');
+	t.is(p.amount, 1000);
+	t.is(p.days, 30);
+	t.is(p.name, 'test-api');
 });
 
 test('create a plan without the mandatory fields', t => {
-    let plan = new Plan();
+	const plan = new Plan();
 
-    t.throws(() => plan.create());
+	t.throws(() => plan.create());
 });
 
 test('create a plan with an id', t => {
-    let plan = new Plan({
-         amount: 1000,
-        days: 30,
-        name: 'test-api',
-        id: 25000
-    });
+	const plan = new Plan({
+		amount: 1000,
+		days: 30,
+		name: 'test-api',
+		id: 25000
+	});
 
-    t.throws(() => plan.create());
+	t.throws(() => plan.create());
 });
 
 test('find all plans', async t => {
-    let plan = new Plan();
+	const plan = new Plan();
 
-    let plans = await plan.findAll();
+	const plans = await plan.findAll();
 
-    t.ok(is.not.undefined(plans));
-    t.ok(is.array(plans));
-    t.is(plans[0].object, 'plan');
+	t.ok(is.not.undefined(plans));
+	t.ok(is.array(plans));
+	t.is(plans[0].object, 'plan');
 });
 
 test('find a plan with an id', async t => {
-    let plan = new Plan({
-        id: 25937
-    });
+	const plan = new Plan({
+		id: 25937
+	});
 
-    let p = await plan.find(); 
+	const p = await plan.find();
 
-    t.ok(is.not.undefined('object'));
-    t.ok(is.object(p));
-    t.is(p.object, 'plan');
-    t.is(p.id, 25937);
+	t.ok(is.not.undefined('object'));
+	t.ok(is.object(p));
+	t.is(p.object, 'plan');
+	t.is(p.id, 25937);
 });
 
 test('find a plan passing an id', async t => {
-    let plan = new Plan();
+	const plan = new Plan();
 
-    let p = await plan.find(25937); 
+	const p = await plan.find(25937);
 
-    t.ok(is.not.undefined('object'));
-    t.ok(is.object(p));
-    t.is(p.object, 'plan');
-    t.is(p.id, 25937);
+	t.ok(is.not.undefined('object'));
+	t.ok(is.object(p));
+	t.is(p.object, 'plan');
+	t.is(p.id, 25937);
 });
 
 test('find a plan with no id', t => {
-    let plan = new Plan();
+	const plan = new Plan();
 
-    t.throws(() => plan.find());
+	t.throws(() => plan.find());
 });
